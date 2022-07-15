744a7f867100bb0ea08e1bd580d03c43e8c8dd77
Gecko310 <viethoang301003@gmail.com>
2022-07-15 17:48:57 +0700

    Update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,14 +1,23 @@
 import { useState, useEffect } from "react";
 function Signup() {
     return (
-        <div className="relative flex flex-col justify-center min-h-screen ">
-            <div className="p-6 bg-slate-200 rounded  m-auto ">
+        <div className="relative flex flex-col bg-slate-200 justify-center min-h-screen ">
+            <div className="p-6 bg-white rounded  m-auto ">
                 <div className="mx-[35%] w-full">
                     Sign Up
                 </div>
-                <input className="p-4 bg-white rounded m-auto">
-
-                </input>
+                <div>
+                <input placeholder="User Name"className="p-4 bg-white rounded m-auto"/>
+                </div>
+                <div>
+                <input placeholder="Email"className="p-4 bg-white rounded m-auto"/>
+                </div>
+                <div>
+                <input placeholder="Password"className="p-4 bg-white rounded m-auto"/>
+                </div>
+                <div>
+                <input placeholder="Confirm Password"className="p-4 bg-white rounded m-auto"/>
+                </div>
             </div>
         </div>
     )
