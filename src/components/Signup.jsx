0c0c166fa69fe555be9f9adb16b7eaf681bd89e6
Gecko310 <viethoang301003@gmail.com>
2022-07-18 21:28:00 +0700

    updatesignup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,22 +1,24 @@
 import { useState, useEffect } from "react";
 function Signup() {
     return (
-        <div className="relative flex flex-col bg-slate-200 justify-center min-h-screen ">
-            <div className="p-6 bg-white rounded  m-auto ">
+        <div className="relative flex flex-col bg-gradient-to-t from-blue-400 to-pink-300 justify-center min-h-screen ">
+            <div className="p-6 bg-white rounded  m-auto  ">
                 <div className="mx-[35%] w-full">
                     Sign Up
                 </div>
-                <div>
-                <input placeholder="User Name"className="p-4 bg-white rounded m-auto"/>
-                </div>
-                <div>
-                <input placeholder="Email"className="p-4 bg-white rounded m-auto"/>
-                </div>
-                <div>
-                <input placeholder="Password"className="p-4 bg-white rounded m-auto"/>
-                </div>
-                <div>
-                <input placeholder="Confirm Password"className="p-4 bg-white rounded m-auto"/>
+                <div className="mx-10">
+                    <div>
+                        <input placeholder="User Name" className="p-4 bg-white rounded mt-5 mb-3 " />
+                    </div>
+                    <div>
+                        <input placeholder="Email" className="p-4 bg-white rounded mb-3" />
+                    </div>
+                    <div>
+                        <input placeholder="Password" className="p-4 bg-white rounded mb-3" />
+                    </div>
+                    <div>
+                        <input placeholder="Confirm Password" className="p-4 bg-white rounded mb-3" />
+                    </div>
                 </div>
             </div>
         </div>
